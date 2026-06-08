f88cb29f9f03c9e57b67a5b5cf682c555321f4f4
Arthur Kunz <arthurhkunz@gmail.com>
2026-06-08 12:32:41 -0300

    chore: add logger config

---
+++ src/modules/shared/logger/winston.config.ts
@@ -0,0 +1,17 @@
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+import * as winston from 'winston';
+
+export const winstonConfig = {
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.errors({ stack: true }),
+        nestWinstonModuleUtilities.format.nestLike('App', {
+          colors: true,
+          prettyPrint: true,
+        }),
+      ),
+    }),
+  ],
+};
